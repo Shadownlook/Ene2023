6d354cb5f60b86f29bd3ec67e9c06c1baf5598b4
SEBASTIAN VIROLDE PARDO <SEBASTIAN.VIROLDE@correoaiep.cl>
2023-09-30 18:12:36 -0300

    Envio de cambios

--- ene2023/Form1.cs
+++ ene2023/Form1.cs
@@ -1,20 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ene2023_1;
 
 namespace ene2023
 {
     public partial class Form1 : Form
     {
+        private UserValidation userValidation;
         public Form1()
         {
             InitializeComponent();
+
+            textBox2.PasswordChar = '*';
+            userValidation = new UserValidation();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            // Lógica de autenticación utilizando UserValidation
+            bool isAuthenticated = userValidation.ValidateUser(username, password);
+
+            if (isAuthenticated)
+            {
+                MessageBox.Show("Inicio de sesión exitoso.");
+                // Puedes abrir la ventana principal de la aplicación o realizar otras acciones aquí.
+            }
+            else
+            {
+                MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+            }
         }
     }
 }
